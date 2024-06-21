9ce0e84158ef34590ae282afd0e0e4cb163c36cd
cesrra <c3z4r.rincon@gmail.com>
2024-06-21 12:37:59 -0500

    Change some error in express validations and change the respond code on create post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -39,7 +39,7 @@ exports.getOnePost = async (req, res, next) => {
 exports.createOnePost = async (req, res, next) => {
     try {        
         const post = await Post.create(req.body)
-        res.status(200).json({
+        res.status(201).json({
             status: "succes",
             data: {
                 post
