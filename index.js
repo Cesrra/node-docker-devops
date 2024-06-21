5cc48c20f163e60220528be2e644824105962820
cesrra <c3z4r.rincon@gmail.com>
2024-06-21 12:39:07 -0500

    Added the user model and the logic to create and login, using password encryption. The service is only to save an get data it's no really auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./src/config/config")
 const postRouter = require("./src/routes/postRoutes")
+const userRouter = require("./src/routes/userRoutes")
 
 const app = express()
 const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
@@ -23,6 +24,7 @@ conectWithRetry()
 app.use(express.json())
 
 app.use("/api/v1/posts", postRouter)
+app.use("/api/v1/users", userRouter)
 
 app.use("/", (req, res) => {
     res.send(`<h1>${process.env.NAME} is Lerning! - And - It is Really Awesome!</h1>`)
@@ -33,11 +35,10 @@ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listing in port ${port}`))
 
 /* DOCKER COMANDS
-docker iamges --> show the docker's images
-docker ps --> show docker's containers that are runing
-docker ps -a --> show docker's containers that are runing and stoped
-docker logs node-app --> show the logs from node-app container
+docker image pull mongo --> get an image of mongo
 docker build -t node-app-image . --> create a image using the Dockerfile in the root and put de name node-app-image
+docker iamges --> show the docker's images
+docker image rm node-app-image --> remove the image with the name node-app-image
 docker run -v $(pwd):/app -p 3000:3000 -d --name node-app node-app-image --> runs the node-app-image, put the name 
     node-app to the instance, listening the changes from the $(pwd) to /app, and listening on port 3000:3000 and
     we have all permisions to CRUD and this changes are afected the -v $(pwd)
@@ -46,10 +47,13 @@ docker run -v $(pwd):/app -v /app/node_modules -p 3000:3000 -d --name node-app n
 docker run -v $(pwd):/app:ro -v /app/node_modules -p 3000:3000 -d --name node-app node-app-image --> it's the same to the previous
     but only give the permission to read with the :ro
 docker run -v $(pwd):/app:ro -v /app/node_modules --env PORT=4000 -p 3000:3000 -d --name node-app node-app-image
+                                                                  Externo:Interno
+docker ps --> show docker's containers that are runing
+docker ps -a --> show docker's containers that are runing and stoped
+docker logs node-app --> show the logs from node-app container
 docker exec -it node-app bash --> execueted a bash to the container node-app
+    printenv --> into the bash show the env variables
 docker rm node-app -f --> remove the node-app continer with focer
-docker image rm node-app-image --> remove the image with the name node-app-image
-printenv --> into the bash show the env variables
 docker volume ls --> list the volumes that you have
 docker volume prune --> deleted all volumes that are not using
 */
@@ -66,8 +70,8 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build --
 */
 
 /* Create a MongoDB
-mongosh -u "dbmongo" -p "p@ssw@rd" --> into the bash of container of mongo, this command loging to DB
-docker exec -it node-docker-devops_mongodb_1 mongosh -u "dbmongo" -p "p@ssw@rd" --> to loging faster
+mongosh -u "dbmongo" -p "p4ssw0rd" --> into the bash of container of mongo, this command loging to DB
+docker exec -it node-docker-devops_mongodb_1 mongosh -u "dbmongo" -p "p4ssw0rd" --> to loging faster
 use mydb --> to use the mydb database
 show dbs --> show the available databases
 db.books.insert({"name": "César Rincón"})
---
+++ src/controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/userModel")
+const bcrypt = require("bcryptjs")
+
+exports.signUp = async (req, res) => {
+    const { password, username } = req.body
+    const hashPassword = await bcrypt.hash(password, 12)
+
+    try {
+        const newUser = await User.create({
+            username,
+            password: hashPassword
+        })
+        res.status(201).json({
+            status: "succes",
+            data: {
+                user: newUser
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            status: "fail"
+        })
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const user = await User.findOne({username})
+
+        if(!user) {
+            return res.status(404).json({
+                status: "fail",
+                message: "User no found!"
+            })
+        }
+
+        const isCorrect = await bcrypt.compare(password, user.password)
+        console.log(isCorrect)
+        if(isCorrect) {
+            res.status(200).json({
+                status: "succes",
+                data: {
+                    user
+                }
+            })
+        }else {
+            res.status(400).json({
+                status: "fail",
+                message: "Incorrecte password!"
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            status: "fail"
+        })
+    }
+}
